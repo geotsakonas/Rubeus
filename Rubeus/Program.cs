14b6d1308745b87894b765f0914d978c84691fcd
Georgios Tsakonas <94357854+geotsakonas@users.noreply.github.com>
2022-05-19 12:27:46 +0300

    Update Program.cs

--- Rubeus/Program.cs
+++ Rubeus/Program.cs
@@ -16,31 +16,6 @@ namespace Rubeus
 
             string file = parsedArgs["/consoleoutfile"];
 
-            TextWriter realStdOut = Console.Out;
-            TextWriter realStdErr = Console.Error;
-
-            using (StreamWriter writer = new StreamWriter(file, true))
-            {
-                writer.AutoFlush = true;
-                Console.SetOut(writer);
-                Console.SetError(writer);
-
-                MainExecute(commandName, parsedArgs);
-
-                Console.Out.Flush();
-                Console.Error.Flush();
-            }
-            Console.SetOut(realStdOut);
-            Console.SetError(realStdErr);
-        }
-
-        private static void MainExecute(string commandName, Dictionary<string,string> parsedArgs)
-        {
-            // main execution logic
-
-            Info.ShowLogo();
-
-            try
             {
                 var commandFound = new CommandCollection().ExecuteCommand(commandName, parsedArgs);
 
@@ -73,6 +48,271 @@ namespace Rubeus
             var commandName = args.Length != 0 ? args[0] : "";
 
             TextWriter realStdOut = Console.Out;
+            TextWriter realStdErr = Console.Error;
+            TextWriter stdOutWriter = new StringWriter();
+            TextWriter stdErrWriter = new StringWriter();
+            Console.SetOut(stdOutWriter);
+            Console.SetError(stdErrWriter);
+
+            MainExecute(commandName, parsed.Arguments);
+
+            Console.Out.Flush();
+            Console.Error.Flush();
+            Console.SetOut(realStdOut);
+            Console.SetError(realStdErr);
+
+            string output = "";
+            output += stdOutWriter.ToString();
+            output += stdErrWriter.ToString();
+
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+            // try to parse the command line arguments, show usage on failure and then bail
+            var parsed = ArgumentParser.Parse(args);
+            if (parsed.ParsedOk == false) {
+                Info.ShowLogo();
+                Info.ShowUsage();
+                return;
+            }
+
+            var commandName = args.Length != 0 ? args[0] : "";
+
+            if (parsed.Arguments.ContainsKey("/nowrap"))
+            {
+                wrapTickets = false;
+            }
+
+            if (parsed.Arguments.ContainsKey("/consoleoutfile")) {
+                // redirect output to a file specified
+                FileExecute(commandName, parsed.Arguments);
+            }
+            else
+            {
+                MainExecute(commandName, parsed.Arguments);
+                TextWriter realStdOut = Console.Out;
+            TextWriter realStdErr = Console.Error;
+            TextWriter stdOutWriter = new StringWriter();
+            TextWriter stdErrWriter = new StringWriter();
+            Console.SetOut(stdOutWriter);
+            Console.SetError(stdErrWriter);
+
+            MainExecute(commandName, parsed.Arguments);
+
+            Console.Out.Flush();
+            Console.Error.Flush();
+            Console.SetOut(realStdOut);
+            Console.SetError(realStdErr);
+
+            string output = "";
+            output += stdOutWriter.ToString();
+            output += stdErrWriter.ToString();
+
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+            // try to parse the command line arguments, show usage on failure and then bail
+            var parsed = ArgumentParser.Parse(args);
+            if (parsed.ParsedOk == false) {
+                Info.ShowLogo();
+                Info.ShowUsage();
+                return;
+            }
+
+            var commandName = args.Length != 0 ? args[0] : "";
+
+            if (parsed.Arguments.ContainsKey("/nowrap"))
+            {
+                wrapTickets = false;
+            }
+
+            if (parsed.Arguments.ContainsKey("/consoleoutfile")) {
+                // redirect output to a file specified
+                FileExecute(commandName, parsed.Arguments);
+            }
+            else
+            {
+                MainExecute(commandName, parsed.Arguments);TextWriter realStdOut = Console.Out;
+            TextWriter realStdErr = Console.Error;
+            TextWriter stdOutWriter = new StringWriter();
+            TextWriter stdErrWriter = new StringWriter();
+            Console.SetOut(stdOutWriter);
+            Console.SetError(stdErrWriter);
+
+            MainExecute(commandName, parsed.Arguments);
+
+            Console.Out.Flush();
+            Console.Error.Flush();
+            Console.SetOut(realStdOut);
+            Console.SetError(realStdErr);
+
+            string output = "";
+            output += stdOutWriter.ToString();
+            output += stdErrWriter.ToString();
+
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+            // try to parse the command line arguments, show usage on failure and then bail
+            var parsed = ArgumentParser.Parse(args);
+            if (parsed.ParsedOk == false) {
+                Info.ShowLogo();
+                Info.ShowUsage();
+                return;
+            }
+
+            var commandName = args.Length != 0 ? args[0] : "";
+
+            if (parsed.Arguments.ContainsKey("/nowrap"))
+            {
+                wrapTickets = false;
+            }
+
+            if (parsed.Arguments.ContainsKey("/consoleoutfile")) {
+                // redirect output to a file specified
+                FileExecute(commandName, parsed.Arguments);
+            }
+            else
+            {
+                MainExecute(commandName, parsed.Arguments);TextWriter realStdOut = Console.Out;
+            TextWriter realStdErr = Console.Error;
+            TextWriter stdOutWriter = new StringWriter();
+            TextWriter stdErrWriter = new StringWriter();
+            Console.SetOut(stdOutWriter);
+            Console.SetError(stdErrWriter);
+
+            MainExecute(commandName, parsed.Arguments);
+
+            Console.Out.Flush();
+            Console.Error.Flush();
+            Console.SetOut(realStdOut);
+            Console.SetError(realStdErr);
+
+            string output = "";
+            output += stdOutWriter.ToString();
+            output += stdErrWriter.ToString();
+
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+            // try to parse the command line arguments, show usage on failure and then bail
+            var parsed = ArgumentParser.Parse(args);
+            if (parsed.ParsedOk == false) {
+                Info.ShowLogo();
+                Info.ShowUsage();
+                return;
+            }
+
+            var commandName = args.Length != 0 ? args[0] : "";
+
+            if (parsed.Arguments.ContainsKey("/nowrap"))
+            {
+                wrapTickets = false;
+            }
+
+            if (parsed.Arguments.ContainsKey("/consoleoutfile")) {
+                // redirect output to a file specified
+                FileExecute(commandName, parsed.Arguments);
+            }
+            else
+            {
+                MainExecute(commandName, parsed.Arguments);TextWriter realStdOut = Console.Out;
+            TextWriter realStdErr = Console.Error;
+            TextWriter stdOutWriter = new StringWriter();
+            TextWriter stdErrWriter = new StringWriter();
+            Console.SetOut(stdOutWriter);
+            Console.SetError(stdErrWriter);
+
+            MainExecute(commandName, parsed.Arguments);
+
+            Console.Out.Flush();
+            Console.Error.Flush();
+            Console.SetOut(realStdOut);
+            Console.SetError(realStdErr);
+
+            string output = "";
+            output += stdOutWriter.ToString();
+            output += stdErrWriter.ToString();
+
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+            // try to parse the command line arguments, show usage on failure and then bail
+            var parsed = ArgumentParser.Parse(args);
+            if (parsed.ParsedOk == false) {
+                Info.ShowLogo();
+                Info.ShowUsage();
+                return;
+            }
+
+            var commandName = args.Length != 0 ? args[0] : "";
+
+            if (parsed.Arguments.ContainsKey("/nowrap"))
+            {
+                wrapTickets = false;
+            }
+
+            if (parsed.Arguments.ContainsKey("/consoleoutfile")) {
+                // redirect output to a file specified
+                FileExecute(commandName, parsed.Arguments);
+            }
+            else
+            {
+                MainExecute(commandName, parsed.Arguments);TextWriter realStdOut = Console.Out;
+            TextWriter realStdErr = Console.Error;
+            TextWriter stdOutWriter = new StringWriter();
+            TextWriter stdErrWriter = new StringWriter();
+            Console.SetOut(stdOutWriter);
+            Console.SetError(stdErrWriter);
+
+            MainExecute(commandName, parsed.Arguments);
+
+            Console.Out.Flush();
+            Console.Error.Flush();
+            Console.SetOut(realStdOut);
+            Console.SetError(realStdErr);
+
+            string output = "";
+            output += stdOutWriter.ToString();
+            output += stdErrWriter.ToString();
+
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+            // try to parse the command line arguments, show usage on failure and then bail
+            var parsed = ArgumentParser.Parse(args);
+            if (parsed.ParsedOk == false) {
+                Info.ShowLogo();
+                Info.ShowUsage();
+                return;
+            }
+
+            var commandName = args.Length != 0 ? args[0] : "";
+
+            if (parsed.Arguments.ContainsKey("/nowrap"))
+            {
+                wrapTickets = false;
+            }
+
+            if (parsed.Arguments.ContainsKey("/consoleoutfile")) {
+                // redirect output to a file specified
+                FileExecute(commandName, parsed.Arguments);
+            }
+            else
+            {
+                MainExecute(commandName, parsed.Arguments);TextWriter realStdOut = Console.Out;
             TextWriter realStdErr = Console.Error;
             TextWriter stdOutWriter = new StringWriter();
             TextWriter stdErrWriter = new StringWriter();
